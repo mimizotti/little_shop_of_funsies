ec15ec3f9647d0a9bd8e83c9e0307fcf77782381
Ricky Amparo <rickyamparo@gmail.com>
2017-09-12 16:56:00 -0600

    add test for creating a user account

---
+++ spec/features/new_user_can_create_account_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+# As a visitor
+feature "New user can create an account" do
+  context "should see a link for create account on root page" do
+    visit '/'
+
+    expect(page).to have_link("Login")
+
+    click_on "Login"
+
+    expect(current_path).to eq(login_path)
+    expect(page).to have_link("Create Account")
+
+    click_on "Create Account"
+
+    fill_in "user[first_name]", with: "Tester"
+    fill_in "user[last_name]", with: "McTesty"
+    fill_in "user[email]", with: "testerson@testmail.com"
+    fill_in "user[password]", with: "testing"
+
+    click_on "Submit"
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content "Logged in as Tester McTesty"
+    expect(page).to have_content "testerson@testmail.com"
+
+    expect(page).to_not have_link "Login"
+    expect(page).to have_link "Logout"
+  end
+end
