4e780a96e34671f32bb09699b5c55502502beced
samuelssnider <sam_redins@yahoo.com>
2017-09-13 17:04:05 -0600

    cleaning up

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,5 @@
 class CategoriesController < ApplicationController
 	def show
-		binding.pry
 		@category = Category.friendly.find(params[:category])
 	end
 end
--- spec/features/guest_can_visit_categories_show_spec.rb
+++ spec/features/guest_can_visit_categories_show_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-RSpec.xdescribe "As a visitor can visit category show page" do
+RSpec.describe "As a visitor can visit category show page" do
 	it " can visit category show page" do
 		one_url = "http://pandathings.com/wp-content/uploads/2016/10/onesie-6-300x300.png"
 		two_url = "https://www.kcmaustralia.com/media/Animal%20Onesie/adventure%20time%20jake%20onesie.jpg"
@@ -9,7 +9,7 @@ RSpec.xdescribe "As a visitor can visit category show page" do
 		item_two = category.items.create(title: "Funsie Twosie", description: "too awesome", price: 12.00,
 		image: two_url )
 		
-		visit '/Animals'
+		visit '/animals'
 		
 	end
 end
