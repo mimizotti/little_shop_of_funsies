44d62397daae1610ba79dcb564929fc2d8a21e46
Ricky Amparo <rickyamparo@gmail.com>
2017-09-12 19:18:41 -0600

    change the way the spec was being represented when you run the command rspec

--- spec/features/new_user_can_create_account_spec.rb
+++ spec/features/new_user_can_create_account_spec.rb
@@ -1,32 +1,30 @@
 require 'rails_helper'
 
 feature "New user can create an account" do
-  context "should see a link for create account on root page" do
-    it "can create an account" do
-      visit '/'
+  scenario "should see a link for create account on root page" do
+    visit '/'
 
-      expect(page).to have_link("Login")
+    expect(page).to have_link("Login")
 
-      click_on "Login"
+    click_on "Login"
 
-      expect(current_path).to eq(login_path)
-      expect(page).to have_link("Create Account")
+    expect(current_path).to eq(login_path)
+    expect(page).to have_link("Create Account")
 
-      click_on "Create Account"
+    click_on "Create Account"
 
-      fill_in "user[first_name]", with: "Tester"
-      fill_in "user[last_name]", with: "McTesty"
-      fill_in "user[email]", with: "testerson@testmail.com"
-      fill_in "user[password]", with: "testing"
+    fill_in "user[first_name]", with: "Tester"
+    fill_in "user[last_name]", with: "McTesty"
+    fill_in "user[email]", with: "testerson@testmail.com"
+    fill_in "user[password]", with: "testing"
 
-      click_on "Submit"
+    click_on "Submit"
 
-      expect(current_path).to eq(dashboard_index_path)
-      expect(page).to have_content "Logged in as Tester McTesty"
-      expect(page).to have_content "testerson@testmail.com"
+    expect(current_path).to eq(dashboard_index_path)
+    expect(page).to have_content "Logged in as Tester McTesty"
+    expect(page).to have_content "testerson@testmail.com"
 
-      expect(page).to_not have_link "Login"
-      expect(page).to have_link "Logout"
-    end
+    expect(page).to_not have_link "Login"
+    expect(page).to have_link "Logout"
   end
 end
