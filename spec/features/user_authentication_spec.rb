915ce75e561ff1252b08155b71feeeaa2fce4fd3
JunePaloma <kborkoski@gmail.com>
2017-09-12 20:12:32 -0600

    Initial story 8 commit with test

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -7,9 +7,28 @@ describe "And when I click “Login” I should be on the “/login page”" do
       describe "Then my current page should be “/dashboard”" do
         it " And I should see a message in the navbar that says “Logged in as SOME_USER” as well as my profile information and I  a link for “Logout, but no link for log_in”" do
 
+          visit '/'
+
+          expect(page).to have_link("Login")
+
+          click_on "Login"
+
+          expect(current_path).to eq(login_path)
+          expect(page).to have_link("Log in")
+
+          click_on "Log in"
+
+
+          expect(current_path).to eq(dashboard_index_path)
+          expect(page).to have_content "Logged in as Tester McTesty"
+          expect(page).to have_content "testerson@testmail.com"
+
+          expect(page).to_not have_link "Login"
+          expect(page).to have_link "Logout"
+
           end
         end
       end
-    end 
+    end
   end
 end
