9c85911c14f83fccb7587de968122b4c68ec49bd
samuelssnider <sam_redins@yahoo.com>
2017-09-12 18:55:21 -0600

    working on category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
 	has_many :items
 	validates :title, presence: true
+	
+	def to_param
+		title.parameterize
+	end
 end
---
+++ spec/features/guest_can_visit_categories_show_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe "As a visitor can visit category show page" do
+	it " can visit category show page" do
+		
+	end
+end
