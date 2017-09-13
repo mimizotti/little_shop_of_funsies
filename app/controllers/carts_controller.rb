6f0d7271c3883cb56f9296b2ac923d8c5d67d19a
Mimi Le <mimilettd@gmail.com>
2017-09-13 09:16:16 -0600

    Removed comments

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -12,11 +12,5 @@ class CartsController < ApplicationController
 
     flash[:notice] = "You now have #{pluralize(@cart.count_of(item.id), item.title)}."
     redirect_to items_path
-    # id = params[:item_id].to_s
-    # item = Item.find(id)
-    # session[:cart] ||= {}
-    # session[:cart][id] = (session[:cart][id] || 0) + 1
-    # flash[:notice] = "You now have #{pluralize(session[:cart][id], item.title)}."
-    # redirect_to items_path
   end
 end
