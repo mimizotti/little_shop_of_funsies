42d9dde2b9b7ab04f68d9e8b07771a40688c5278
Ricky Amparo <rickyamparo@gmail.com>
2017-09-13 16:06:15 -0600

    remove commented out user tory form the feature test

--- spec/features/visitor_can_increase_item_quantity_in_cart_spec.rb
+++ spec/features/visitor_can_increase_item_quantity_in_cart_spec.rb
@@ -26,11 +26,3 @@ feature "Visitor can increase an item's quantity in the cart" do
     expect(@cart.total).to eq(39.98)
   end
 end
-
-# When I visit “/cart”
-# Then I should see my item with a quantity of 1
-# And when I increase the quantity
-# Then my current page should be ‘/cart’
-# And that item’s quantity should reflect the increase
-# And the subtotal for that item should increase
-# And the total for the cart should match that increase
