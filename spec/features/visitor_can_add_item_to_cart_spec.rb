0eb2b5c0af12354d284cebf34c93e89e45b2ec9e
mimilettd <mimilettd@gmail.com>
2017-09-12 21:57:37 -0600

    broke test into several tests

--- spec/features/visitor_can_add_item_to_cart_spec.rb
+++ spec/features/visitor_can_add_item_to_cart_spec.rb
@@ -5,12 +5,20 @@ RSpec.feature "Adding items to the cart" do
 
     create_items
 
+    visit items_path
+
   end
-  context "As a visitor" do
-    it "I should be able to add items to cart" do
-      visit items_path
+  context "When a visitor adds items to their cart" do
+    it "a message is displayed" do
 
       expect(page).to have_link("Add to cart")
+
+      click_on "Add to cart"
+
+      expect(page).to have_content("You now have 1 Black Cat Onesie")
+
+    end
+    it "the message correctly increments for multiple items" do
 
       click_on "Add to cart"
 
@@ -19,17 +27,25 @@ RSpec.feature "Adding items to the cart" do
       click_on "Add to cart"
 
       expect(page).to have_content("You now have 2 Black Cat Onesies")
+    end
+    it "the total number of items in the cart increments" do
 
-      click_on "Cart"
+      expect(page).to have_content("Cart: 0")
 
-      expect(current_path).to eq(cart_path)
-      expect(page).to have_css("img[src=\"#{@item.image}\"]")
-      expect(page).to have_content(@item.name)
-      expect(page).to have_content(@item.description)
+      click_on "Add to cart"
 
-      within '.total' do
-        page.should have_content "$19.99"
-      end
+      expect(page).to have_content("Cart: 1")
+    end
+      #
+      # click_on "Cart"
+      #
+      # expect(current_path).to eq(cart_path)
+      # expect(page).to have_css("img[src=\"#{@item.image}\"]")
+      # expect(page).to have_content(@item.name)
+      # expect(page).to have_content(@item.description)
+      #
+      # within '.total' do
+      #   page.should have_content "$19.99"
     end
   end
 end
