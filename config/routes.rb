5616d0ae57c1cd7d06fa8885bf61a89fc38b20fa
Ricky Amparo <rickyamparo@gmail.com>
2017-09-12 19:40:06 -0600

    Remove comments from routes file

--- config/routes.rb
+++ config/routes.rb
@@ -5,28 +5,12 @@ Rails.application.routes.draw do
   get '/login', :to => 'sessions#new', :as => 'login'
   post '/login', :to => 'sessions#create'
   delete '/logout', :to => 'sessions#destroy'
-  # get '/dashboard', :to => 'users#show', :as => 'dashboard'
 
   resources :users, only: [:new, :create]
 
   resources :dashboard, only: [:index]
 
-  # namespace :dashboard do
-  #   resources :user, only: [:show]
-  # end
-
-  # scope(path_names: { show: ('dashboard')}) do
-  #   resources :users, path: 'dashboard', only: :show
-  # end
-  #
-  # resources :users, only: [:show] do
-  #   collection do
-  #     get :dashboard
-  #   end
-  # end
-
-
-	resources :items, only: [:index, :show]
-	resources :categories, only: [:show]
+resources :items, only: [:index, :show]
+resources :categories, only: [:show]
 
 end
