9f7e0b63bdc949a06ca766ee7550b39679b395c1
mimilettd <mimilettd@gmail.com>
2017-09-12 21:07:31 -0600

    update Carts#Create to include pluralization when pushing out flash message

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,12 @@
 class CartsController < ApplicationController
+  include ActionView::Helpers::TextHelper
 
   def create
-    item = Item.find(params[:item_id])
-    flash[:notice] = "You now have 1 #{item.title}."
+    id = params[:item_id].to_s
+    item = Item.find(id)
+    session[:cart] ||= {}
+    session[:cart][id] = (session[:cart][id] || 0) + 1
+    flash[:notice] = "You now have #{pluralize(session[:cart][id], item.title)}."
     redirect_to items_path
   end
 end
