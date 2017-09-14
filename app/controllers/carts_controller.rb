1c57934ed5eedc8ff775ea7d1a1b8d9a5a02b36d
Ricky Amparo <rickyamparo@gmail.com>
2017-09-13 20:27:55 -0600

    add spec to user can increase item quantity in the cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -13,6 +13,12 @@ class CartsController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
+  def increase
+    item_id = params[:format]
+    @cart.increase_quantity_item(item_id)
+    redirect_to '/cart'
+  end
+
   def decrease
     item_id = params[:format]
     @cart.decrease_quantity_item(item_id)
--- spec/features/visitor_can_increase_item_quantity_in_cart_spec.rb
+++ spec/features/visitor_can_increase_item_quantity_in_cart_spec.rb
@@ -8,21 +8,21 @@ feature "Visitor can increase an item's quantity in the cart" do
   end
 
   scenario "visitor has an item in the cart and then she increases it to 2" do
-    visit '/cart'
+    visit items_path
 
+    click_on "Add to cart"
+
+    visit carts_path
     expect(page).to have_content(@item.title)
     expect(page).to have_content(1)
 
-    click_on "Increase"
+    click_on "Increase quantity"
 
     expect(current_path).to eq('/cart')
     expect(page).to have_content(@item.title)
+
     expect(page).to have_content(2)
-
-    expect(@cart.total_count).to eq(2)
-
-    expect(@cart.subtotal).to eq(39.98)
-
-    expect(@cart.total).to eq(39.98)
+    expect(page).to have_content(39.98)
+    expect(page).to have_content("Subtotal: $39.98")
   end
 end
