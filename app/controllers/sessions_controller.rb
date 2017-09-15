2e4ca1206f7b75629c47d5530ac48dc2fd9ac904
JunePaloma <kborkoski@gmail.com>
2017-09-15 16:12:47 -0600

    Add if elsif condition to sessions controller create method to allow for redirect to admin dashboard

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,18 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       flash[:notice] = "Logged in as #{@user.first_name} #{@user.last_name}"
+      if @user.role == "admin"
+        redirect_to admin_dashboards_path
+      elsif @user.role == "default"
       redirect_to dashboard_index_path
+      end 
     else
       flash[:failure] = "That login was unsuccessful"
       redirect_to login_path
+
     end
   end
-  
+
   def destroy
     session.clear
     redirect_to root_path
--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -1,7 +1,6 @@
-
 require 'rails_helper'
 
-  xdescribe "admin dashboard feature" do
+  describe "admin dashboard feature" do
     describe "As a logged in Admin user" do
       describe "when I visit /admin/dashboard" do
         it "I will see a heading on the page that says Admin Dashboard" do
@@ -10,21 +9,21 @@ require 'rails_helper'
 
           allow_any_instance_of(ApplicationController).to receive(:current_user). and_return(admin_user)
 
-          visit admin_dashboard_path
+          visit admin_dashboards_path
           expect(page).to have_content("Admin Dashboard")
 
       end
     end
   end
 
-  xdescribe "as a logged in user when I visit /admin/dashboard" do
+  describe "as a logged in user when I visit /admin/dashboard" do
     it "I see a 404 error" do
 
-      default_user = User.create(first_name: "Admin", last_name: "McAdmin", email: "admin@admin.com" password: "boom")
+      default_user = User.create(first_name: "Admin", last_name: "McAdmin", email: "admin@admin.com", password: "boom")
 
       allow_any_instance_of(ApplicationController).to receive(:current_user). and_return(default_user)
 
-      visit admin_dashboard_path
+      visit admin_dashboards_path
 
       expect(page).to have_content("404")
 
@@ -32,10 +31,10 @@ require 'rails_helper'
   end
 
 
-  xdescribe "as a visitor when I visit /admin/dashboard" do
+  describe "as a visitor when I visit /admin/dashboard" do
     it "I see a 404 error" do
 
-      visit admin_dashboard_path
+      visit admin_dashboards_path
 
       expect(page).to have_content("404")
 
@@ -43,19 +42,21 @@ require 'rails_helper'
   end
 end
 
-xfeature "as an Admin" do
+feature "as an Admin" do
   describe "when I log into my account" do
     it "I am redirected to the Admin Dashboard" do
-      admin = User.create(username: "Admin", password: "boom", role: "admin")
+      admin = User.create(first_name: "Admin", last_name:"McAdmin", email: "admin@email", password: "boom", role: "admin")
 
       visit login_path
-      fill_in "[session]email", with: admin.email
-      fill_in "[session]password", with: admin.password
-      click_on "Login"
 
+      fill_in "session[email]", with: admin.email
+      fill_in "session[password]", with: admin.password
+      within(".action") do
+        click_on("Login")
+      end
+      
       expect(page).to have_content("Admin Dashboard")
-      expect(current_path).to eq(admin_dashboard_path)
+      expect(current_path).to eq(admin_dashboards_path)
     end
   end
 end
-
