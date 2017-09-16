f596807bda21e5c97708b58a28c2d7a3885b5f51
JunePaloma <kborkoski@gmail.com>
2017-09-16 12:54:46 -0600

    Refector update method on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,19 +12,16 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
   def update
-    user = User.find(params[:id])
-    if user == current_user && current_admin?
-      user.update(user_params)
+    if current_admin?
+      current_user.update(user_params)
       redirect_to admin_dashboards_path
-    elsif user == current_user
-      user.update(user_params)
-      redirect_to dashboard_index_path
     else
-      render file: "/public/404"
+      current_user.update(user_params)
+      redirect_to dashboard_index_path
     end
   end
 
