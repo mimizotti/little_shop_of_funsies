462594bb670ba04be661ca2d693fbffb7ac098f5
samuelssnider <sam_redins@yahoo.com>
2017-09-16 12:58:42 -0600

    story 24 done, tests passing

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,11 @@
+class Admin::ItemsController < ApplicationController
+before_action :require_admin
+  def index
+    @items = Item.all
+  end
+
+private
+  def require_admin
+    render file: "/public/404" unless current_admin?
+	 end
+end
