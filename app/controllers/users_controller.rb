c7ab736f5d25ec817566269fab4acfdca9575531
JunePaloma <kborkoski@gmail.com>
2017-09-15 18:56:17 -0600

    Add update user route and an update user action to Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,16 @@ class UsersController < ApplicationController
   end
 
   def update
+    user = User.find(params[:id])
+    if user == current_user && current_admin?
+      user.update(user_params)
+      redirect_to admin_dashboards_path
+    elsif user == current_user
+      user.update(user_params)
+      redirect_to dashboard_index_path
+    else
+      render file: "/public/404"
+    end
   end
 
   def show
--- spec/features/admin_can_update_their_account_but_not_user_account_spec.rb
+++ spec/features/admin_can_update_their_account_but_not_user_account_spec.rb
@@ -14,9 +14,9 @@ describe "As a logged in Admin" do
     fill_in "user[password]", with: "testing"
     fill_in "user[email]", with: "testerson@testmail.com"
 
-    click_on "Update"
+    click_on "Submit"
 
-    expect(page).to have_content("mctester@mail.com")
+    expect(page).to have_content("testerson@testmail.com")
 
   end
 
@@ -28,6 +28,9 @@ describe "As a logged in Admin" do
     allow_any_instance_of(ApplicationController).to receive(:current_user). and_return(admin_user)
 
     visit dashboard_index_path(user)
+  
+
+    click_on "Update your account"
 
     expect(page).to have_content("404")
   end
