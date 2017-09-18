fb18789956b4b05a733a95318b2095914a34d893
JunePaloma <kborkoski@gmail.com>
2017-09-17 18:57:44 -0600

    Add feature test for analytics

---
+++ spec/features/admin_can_view_analytics_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe "As an admin when I visit the dashboard" do
+  describe " I see a table" do
+    it " that ranks our items by popularity" do
+      it " I can see the total sales for each item" do
+        it "I can see the total count sold for each item" do
+        end
+      end
+    end
+  end
+
+  describe "I see a table" do
+    it "ranks our customers by total spent" do
+      it "I see the customer's id" do
+        it "I see the customer's name" do
+          it "I see the total number of orders for that customer" do
+          end
+        end
+      end
+    end 
+end
