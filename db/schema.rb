b54ef0ecd089bd04a6bac07431a9c1ddee717faa
mimilettd <mimilettd@gmail.com>
2017-09-19 13:06:34 -0600

    move item table above category table

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,6 @@ ActiveRecord::Schema.define(version: 20170918203915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string "title"
-    t.string "slug"
-    t.index ["slug"], name: "index_categories_on_slug", unique: true
-  end
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -37,6 +32,12 @@ ActiveRecord::Schema.define(version: 20170918203915) do
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.string "slug"
+    t.index ["slug"], name: "index_categories_on_slug", unique: true
+  end
+  
   create_table "order_items", force: :cascade do |t|
     t.bigint "item_id"
     t.bigint "order_id"
