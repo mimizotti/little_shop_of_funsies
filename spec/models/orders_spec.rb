53d6c54262f1f55ddf977c00d583842570aa8cc2
Ricky Amparo <rickyamparo@gmail.com>
2017-09-19 19:54:55 -0600

    add test for order's date method

--- spec/models/orders_spec.rb
+++ spec/models/orders_spec.rb
@@ -65,5 +65,12 @@ RSpec.describe Order do
 
 			expect(order.items.first).to eq(item)
 		end
+
+		it "can return the order date" do
+			user = User.create!(first_name: "Testy", last_name: "McTest", password: "testing", email: "tester@testmail")
+			order = user.orders.create!(status: "ordered", created_at: "2017-09-13 01:13:04 -0600")
+
+			expect(order.date).to eq("Sep. 13, 2017")
+		end
 	end
 end
