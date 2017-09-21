cc67b328d2bb0eb6a865c7e24262ea173a4d786c
mimilettd <mimilettd@gmail.com>
2017-09-20 22:34:42 -0600

    refactored admin analytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require Chart.min
 //= require rails-ujs
 //= require jquery3
 //= require popper
